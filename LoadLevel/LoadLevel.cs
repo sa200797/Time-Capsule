aa84857b89a049f7d69e1358fda375d3025f0ef0
sa200797 <35431254+sa200797@users.noreply.github.com>
2019-07-15 14:43:26 +0530

    Add files via upload

---
+++ LoadLevel/LoadLevel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour
+{
+    public GameObject loadingScreen;
+   // public Image load_image;
+    public Slider slider;
+
+   // public Text p_text;
+
+    private AsyncOperation async;
+    
+  public void Start()
+    {
+        StartCoroutine(LoadMyScence());
+    }
+
+
+    IEnumerator LoadMyScence()
+    {
+        async = SceneManager.LoadSceneAsync(1);
+        while(!async.isDone)
+        {
+            float progress = Mathf.Clamp01(async.progress / .9f);
+            slider.value = async.progress;
+
+         //   p_text.text = async.progress.ToString()  ;
+
+            yield return null;
+        }
+
+    }
+
+
+
+
+
+
+    /*  AsyncOperation operation = SceneManager.LoadSceneAsync(1);
+
+    loadingScreen.SetActive(true);
+
+    while (!operation.isDone)
+    {
+        float progress = Mathf.Clamp01(operation.progress / .9f);
+
+        //load_image.fillAmount = progress;
+        slider.value = operation.progress;
+
+        yield return null;
+    }
+    */
+}
